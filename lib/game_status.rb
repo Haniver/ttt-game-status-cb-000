61ebcdbf97b910b366c716fe1ffd04f33f0d8cb0
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2018-03-14 16:04:42 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -13,3 +13,12 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [2,4,6]
 ]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |winning_line|
+    if board[winning_line[0]] != "" && board[winning_line[0]] != " " && board[winning_line[0]] == board[winning_line[1]] && board[winning_line[1]] == board[winning_line[2]]
+      return true
+    end
+    return false
+  end
+end
